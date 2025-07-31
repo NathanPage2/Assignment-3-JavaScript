48de277104627f5d99fcc069597996c67e31421d
Nathan Page <200609542@student.georgianc.on.ca>
2025-07-31 12:11:15 -0400

    Somehow also forgot to put my html in a body tag, also added my Marvel keys

--- js/api.js
+++ js/api.js
@@ -1,2 +1,4 @@
 const searchBtn = document.querySelector("#search");
 const results = document.querySelector("#results");
+const publicKey = "1d3c25ab6340a7452103a86e2401dd58";
+const privateKey = "2a88640658c32ca6ecd9fbea589b930a943653e6";
