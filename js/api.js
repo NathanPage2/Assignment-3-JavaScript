34328ad1ba4ef27d3aa5051fe50a7f2df1af57c8
Nathan Page <200609542@student.georgianc.on.ca>
2025-07-31 15:02:51 -0400

    I tried another but since it has no cors I can't, i'm switching to a news api but why even bother with a name if this one also doesn't work

--- js/api.js
+++ js/api.js
@@ -1,23 +1,18 @@
-//const ts = Date.now().toString();
-//Had to switch methods because marvel didn't like the hard coding process
+//Marvel didn't fricking work so now I gotta fix so much to use another api that I am praying works
+//That api didn't so now I need to switch topics completly 
 const searchBtn = document.querySelector("#search");
 const results = document.querySelector("#results");
-const publicKey = "1d3c25ab6340a7452103a86e2401dd58";
-const privateKey = "2a88640658c32ca6ecd9fbea589b930a943653e6";
-//const hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
+const accessToken = "48ac86e120cafb6aab78e30ae5784f08";
 document.getElementById("search").addEventListener("click", async () => {
   const characterName = document.getElementById("characterName").value.trim();
-  const ts = Date.now().toString();
-  const hash = CryptoJS.MD5(ts + privateKey + publicKey).toString();
 try{
- const url = `https://gateway.marvel.com/v1/public/characters?name=${encodeURIComponent(characterName)}&ts=${ts}&apikey=${publicKey}&hash=${hash}`;
-  console.log("Fetching:", url);
+ const url = `https://superheroapi.com/api/${accessToken}/search/${encodeURIComponent(characterName)}`;
  const response = await fetch (url);
-  const marvelData = await response.json();
+  const superData = await response.json();
 
   results.innerHTML = "";
 
-  if (!marvelData.data.results.length){
+ if (superData.response === "error" || superData.results.length === 0) {
     //Meaning that there is nothing found
     const p = document.createElement("p");
     //Create a p element
@@ -25,17 +20,18 @@ try{
     results.appendChild(p);
     return;
   }
-  const character = marvelData.data.results[0];
+  const character = superData.results[0];
   //Fetching the character after they are searched
   const img = document.createElement("img");
-  const characterImg = `${character.thumbnail.path}/standard_fantastic.${character.thumbnail.extension}`;
-  img.src = characterImg;
-  img.alt = characterName;
-   results.appendChild(img);
+    img.src = character.image.url;
+    img.alt = character.name;
+    results.appendChild(img);
   //grabbing the path and extension for there api
   const desc = document.createElement("p");
-  desc.textContent = character.description;
-  results.appendChild(desc);
+    desc.textContent = `Full Name: ${character.biography["full-name"] || "N/A"}
+                      \nPublisher: ${character.biography.publisher || "N/A"}
+                      \nAlignment: ${character.biography.alignment || "N/A"}`;
+    results.appendChild(desc);
   //grabbing there description
 } catch {
     const p = document.createElement("p");
